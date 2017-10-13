e9c920f9d26d9782760cb8c42d10984e16936bd7
Soheil Rashidi <contact@soheilrashidi.com>
2017-10-13 10:32:50 +0330

    Added defaults for new sub-issues.

--- projects/web/src/client/app/issues/new-sub-issue-command.ts
+++ projects/web/src/client/app/issues/new-sub-issue-command.ts
@@ -1,9 +1,11 @@
+import * as _ from 'underscore';
 import { IIssue } from '../application';
 import { BaseCommand } from '../commands';
 import { KeyCode } from '../keyboard';
 import { ServiceManager } from '../services';
 
 export default class NewSubIssueCommand extends BaseCommand {
+  private application = ServiceManager.Instance.getApplication();
   private issueController = ServiceManager.Instance.getIssueController();
 
   constructor(private parentIssue: IIssue) {
@@ -27,6 +29,14 @@ export default class NewSubIssueCommand extends BaseCommand {
   }
 
   execute() {
-    this.issueController.addIssue({}, this.parentIssue);
+    const issue: IIssue = {
+      type: _.find(this.application.itemTypes.getAll('issue'), itemType => itemType.key === 'task'),
+      state: _.find(this.application.itemStates.getAll('issue'), itemType => itemType.key === 'todo'),
+      project: this.parentIssue.project,
+      assignedTo: this.parentIssue.assignedTo,
+      milestone: this.parentIssue.milestone,
+    };
+
+    this.issueController.addIssue(issue, this.parentIssue);
   }
 }
